6e22efbee789d324bbd7d20a3aa74efba8bd5861
Nils Rohr <nrohr@rhrk.uni-kl.de>
2020-05-16 14:37:42 +0200

    moved dimensions to useWindowSize for both web and phones

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, } from 'react-native';
+import { Platform, Dimensions } from 'react-native';
 
 export default function useWindowSize(friendID) {
-  let [dimensions, setDimension] = useState( null );
+  let [dimensions, setDimension] = useState( { width: Dimensions.get("screen").width, height: Dimensions.get("screen").height } );
 
-  useEffect ( () => {
-    if (Platform.OS == "web"){
-      setDimension({width:  document.body.clientWidth, height: document.body.clientHeight })
-      window.addEventListener('resize', (data) => {setDimension({width:  document.body.clientWidth, height: document.body.clientHeight })}); 
-    }
-  }, [] )
+  if (Platform.OS == "web"){
+    useEffect ( () => {
+        setDimension({width:  document.body.clientWidth, height: document.body.clientHeight })
+        window.addEventListener('resize', (data) => {setDimension({width:  document.body.clientWidth, height: document.body.clientHeight })}); 
+    }, [] )
+  }
+  else {
+    let screenDimensions = Dimensions.get("screen") || {};
+    console.log("ScreenDimensions", screenDimensions);
+    useEffect ( () => {
+      setDimension({width: screenDimensions.width, height: screenDimensions.height });
+    }, [screenDimensions.width, screenDimensions.height] )
+  }
 
   return dimensions;
 }
--- src/screens/Auth/WelcomeScreen.js
+++ src/screens/Auth/WelcomeScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { connect } from "react-redux"; //Gets the data from the store and pushes them into the this.props of the component
-import { StatusBar, Image, TextInput, Platform, KeyboardAvoidingView, Dimensions, StyleSheet, View, Text, } from 'react-native';
+import { ScrollView, StatusBar, Image, TextInput, Platform, KeyboardAvoidingView, Dimensions, StyleSheet, View, Text, } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 import Button from "../../reuse/Button.js";
@@ -17,8 +17,7 @@ import useWindowSize from "../../hooks/useWindowSize.js";
 import translate from '../../constants/language/languages.js';
 
 function WelcomeScreen( { navigation, route, ...props} ){
-  let dimensions = useWindowSize(); //Only works in browser
-  let {width, height} = dimensions || {width: Dimensions.get("screen").width, height: Dimensions.get("screen").height };
+  let {width, height} = useWindowSize(); //Only works in browser
   let { containerHeight, containerWidth } = { containerHeight: Dimensions.get("window").width, containerWidth: Dimensions.get("window").height  }
 
   let { colors, styles: defaultStyles, icons, fonts } = getTheme();
@@ -30,70 +29,73 @@ function WelcomeScreen( { navigation, route, ...props} ){
       alignItems: "center",
       backgroundColor: colors.background,
     },
+    scrollViewStyle: { 
+      flex: 1,
+      width,
+    },
     imageBG: {
       width: width,
       height: height,
       resizeMode: "cover",
       opacity: 0.5,
+      position: "absolute"
     },
     imageStyle: {
-      maxHeight: height * 0.5,
+      maxHeight: height * 0.4,
       minWidth: 300,
       width: width - 100,
       height: width - 100,
       resizeMode: "contain",
-      position: "absolute",
-      top: height * 0.1,
+      marginTop: height * 0.1,
     },
     elementContainer: {
-      position: "absolute",
-      top: height * 0.50,
       marginTop: width > 400 ? height * 0.15: 0,
-      marginHorizontal: 20,
-      height: height - height * 0.55 - ( Platform.OS != "web" ? (height - containerHeight) : 0 ),
+      marginHorizontal: Platform.OS == "web" ? 15 : 20,
+      minheight: height - height * 0.55 - ( Platform.OS != "web" ? (height - containerHeight) : 0 ),
       justifyContent: "flex-start", 
       alignItems: "center", 
+      flex: 1,
     },
     introTextStyle: {
       color: "white",
-      //marginTop: height* 0.10,
       marginBottom: height * 0.10,
       fontFamily: fonts.standard,
       fontSize: 16,
     },
   }
-  console.log(width > 400 ? width > height ? height * 0.15: height * 0.15 - (height - 400  - width) : 0,)
 
   return(
     <SafeAreaView style={[ styles.containerStyle ]} >
+      <StatusBar translucent backgroundColor={colors.backgroundColor} barStyle={"light-contents"}/>
       <Image
         style={[ styles.imageBG ]}
         source={require('../../../assets/testBGImage.jpg')}
       />
-      <StatusBar translucent backgroundColor={colors.backgroundColor} barStyle={"light-contents"}/>
-      <Image
-        style={[ styles.imageStyle ]}
-        source={require('../../../assets/Logo/cover-white.png')}
-      />
-      <View style={[ styles.elementContainer ]}>
-        <Text style={[styles.introTextStyle]}> {translate("WelcomeScreen_WelcomeText")} </Text>
-        <Button
-          onPress={ () => { navigation.dispatch(openSignInScreen()) } }
-          title={translate( "WelcomeScreen_Sign_In" )}
-          colors={ [colors.primary, colors.primaryVariants[4], colors.primaryVariants[2],] }
-          textStyle={[{color: "white"}]}
-          primary
-          rounded
+      <ScrollView contentContainerStyle={{ alignItems: "center", height, flex: 1}} style={[ styles.scrollViewStyle ]}>
+        <Image
+          style={[ styles.imageStyle ]}
+          source={require('../../../assets/Logo/cover-white.png')}
         />
-        <View style={[{minHeight: 20}]}/>
-        <Button
-          onPress={ () => { navigation.dispatch(openSignUpScreen()) } }
-          title={translate( "WelcomeScreen_Sign_Up" )}
-          primary
-          transparent
-          block
-        />
-      </View>
+        <View style={[ styles.elementContainer ]}>
+          <Text style={[styles.introTextStyle]}> {translate("WelcomeScreen_WelcomeText")} </Text>
+          <Button
+            onPress={ () => { navigation.dispatch(openSignInScreen()) } }
+            title={translate( "WelcomeScreen_Sign_In" )}
+            colors={ [colors.primary, colors.primaryVariants[4], colors.primaryVariants[2],] }
+            textStyle={[{color: "white"}]}
+            primary
+            rounded
+          />
+          <View style={[{minHeight: 20}]}/>
+          <Button
+            onPress={ () => { navigation.dispatch(openSignUpScreen()) } }
+            title={translate( "WelcomeScreen_Sign_Up" )}
+            primary
+            transparent
+            containerStyle={{ marginBottom: 20 }}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
