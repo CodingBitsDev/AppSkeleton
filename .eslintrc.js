9e2c4e79bc67c85b81edda4cf5d491695adac229
Nils Rohr <nrohr@rhrk.uni-kl.de>
2020-05-20 17:36:13 +0200

    added eslingrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "react-native/react-native": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+    "ecmaVersion": 11,
+    "sourceType": "module",
+  },
+  "plugins": [
+    "react",
+    "react-native",
+  ],
+  "rules": {
+    "no-unused-vars" : 0,
+    "react-native/no-unused-styles": 1,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 0,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
+    "react-native/no-single-element-style-arrays": 2,
+  }
+};
