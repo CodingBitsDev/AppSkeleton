27e4ff819e574bdd4c132f30eb552dd5ab272f08
Nils Rohr <nrohr@rhrk.uni-kl.de>
2020-06-07 10:58:27 +0200

    Made sure storybook is also published

--- App.js
+++ App.js
@@ -5,5 +5,8 @@ import StorybookUI from "./storybook/index.js";
 import Main from "src/Main.js";
 
 export default function App() {
+  //Remove this line if you want to remove storybook in your published apps
+  return <StorybookUI />
+
   return __DEV__ ? <StorybookUI /> : <Main />
 }
